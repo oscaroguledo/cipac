ebc7d773b66d0daf76aa483a494375b3b4560f21
Oscar Oguledo <122152636+oscaroguledo@users.noreply.github.com>
2023-07-10 22:37:43 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -170,10 +170,3 @@ EMAIL_HOST = os.environ.get ('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get ('EMAIL_HOST_USER')  
 EMAIL_HOST_PASSWORD = os.environ.get ('EMAIL_HOST_PASSWORD')   
 EMAIL_PORT = os.environ.get ('EMAIL_PORT') 
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = "oscarchiagoziem@gmail.com"
-# EMAIL_HOST_PASSWORD = "ihkqjckjevbpwgkk"
